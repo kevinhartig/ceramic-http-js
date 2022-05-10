8b307775517d9f227354c5b111bc92e724ad4916
khartig <kevin.hartig@gmail.com>
2022-05-09 18:51:06 -0700

    sample ceramic did creation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { CeramicClient } from '@ceramicnetwork/http-client'
+import { DID } from 'dids'
+import { getResolver as getKeyResolver } from 'key-did-resolver'
+import { getResolver as get3IDResolver } from '@ceramicnetwork/3id-did-resolver'
 
 function App() {
+  const API_URL = 'http:///localhost:7007';
+  const ceramic = new CeramicClient(API_URL);
+  const resolver = {
+    ...getKeyResolver(),
+    ...get3IDResolver(ceramic),
+  }
+  const did = new DID({
+    resolver: {
+      // A Ceramic client instance is needed by the 3ID DID resolver to load DID documents
+      ...get3IDResolver(ceramic),
+      // `did:key` DIDs are used internally by 3ID DIDs, therefore the DID instance must be able to resolve them
+      ...getKeyResolver(),
+    },
+  })
+
+  ceramic.did = did;
+  console.log("did = " + ceramic.did);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Basic DID creation
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
